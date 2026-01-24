8acf2c69e285a6d7dbf8638aad6d1faba3aab2c9
Michael Budiputra <michaelbudiputra@gt.student.pens.ac.id>
2026-01-24 13:24:48 +0700

    Mendorong Object

--- gim-jam-2026/Scripts/tembok_bergerak.gd
+++ gim-jam-2026/Scripts/tembok_bergerak.gd
@@ -1,31 +1,20 @@
-extends Node2D
-
-const TILE_SET: Vector2 = Vector2(16,16)
+extends GridObject
 
 @export_enum("Atas", "Bawah", "Kanan", "Kiri") var letak := 0
-
-@export_group("Timer")
 @export var timer: Timer
-@export var timeout: int
+@export var timeout: float = 1.0
 
-var direction
-var move_dir: Vector2
+var direction: Vector2
 
 func _ready():
 	timer.wait_time = timeout
 	timer.start()
-	if letak == 2 || letak == 3:
-		rotation_degrees = 90
+
 	match letak:
-		0:
-			direction = Vector2.DOWN
-		1:
-			direction = Vector2.UP
-		2:
-			direction = Vector2.LEFT
-		3:
-			direction = Vector2.RIGHT
+		0: direction = Vector2.DOWN
+		1: direction = Vector2.UP
+		2: direction = Vector2.LEFT
+		3: direction = Vector2.RIGHT
 
-
-func _on_timer_timeout() -> void:
-	global_position += direction * TILE_SET
+func _on_timer_timeout():
+	try_move(direction)
