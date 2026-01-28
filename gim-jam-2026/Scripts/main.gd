48f3e0d6681737ca39e635c16157e6e5dcc7d0e0
Michael Budiputra <michaelbudiputra@gt.student.pens.ac.id>
2026-01-28 13:47:51 +0700

    add stage checklist

--- gim-jam-2026/Scripts/main.gd
+++ gim-jam-2026/Scripts/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var score_label: Label
 @export_file_path("*.tscn") var main_menu: String
 @export_file_path("*.tscn") var stage2: String
+@export var stage = Global.stage
 var last_score_value := 0
 
 func _ready() -> void:
@@ -10,6 +11,7 @@ func _ready() -> void:
 	# Initialize stored value
 	last_score_value = Global.score
 	_update_score_label()
+	
 
 func _process(_delta: float) -> void:
 	# Check for change
@@ -19,12 +21,12 @@ func _process(_delta: float) -> void:
 	last_score_value = Global.score
 	_update_score_label()
 	
-	if Global.score == 4 and Global.stage == Global.State.STAGE1:
+	if Global.score == 4 and stage == Global.State.STAGE1:
 		print("Stage 1 Clear")
 		Global.score = 0
 		Global.stage = Global.State.STAGE2
 		get_tree().change_scene_to_file(stage2)
-	elif Global.score == 4 and Global.stage==Global.State.STAGE2:
+	elif Global.score == 4 and stage==Global.State.STAGE2:
 		print("Stage 2 Clear")
 		Global.score = 0
 		Global.stage = Global.State.STAGE1
